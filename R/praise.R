9c2394331a1524eb9163f1d5f84efab7f2c3629a
alexander-russell <alexander.russell4@pm.me>
2023-08-27 10:58:51 +1000

    Initial commit

---
+++ R/praise.R
@@ -0,0 +1,17 @@
+#' Deliver praise
+#'
+#' @description
+#' This function returns a string of praise.
+#'
+#'
+#' @param name text string; containing a name
+#' @param punctuation text string; the symbol to end the message with following the name.
+#'
+#' @return text string with a phrase containing the name and punctuation
+#' @export
+#'
+#' @examples
+#' praise(name = "Steph", punctuation = "!")
+praise <- function(name, punctuation = "!"){
+  glue::glue("You're the best, {name}{punctuation}")
+}
